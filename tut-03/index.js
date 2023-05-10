fbe8c2c055b2b4148d2c46c4b1ce1bcd16129350
DevBigEazi <adesholatajudeen1@gmail.com>
2023-05-10 11:05:39 +0100

    tut-03 done

--- tut-03/index.js
+++ tut-03/index.js
@@ -1,1 +1,7 @@
-console.log("Testing");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+console.log(format(new Date(), "yyyy-MM-dd\tHH:mm:ss"));
+console.log(uuid());
+
+console.log("Hello");
