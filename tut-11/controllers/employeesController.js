02e9be2f1db4d38cbb4eb82d0f6baae1d3dea17f
DevBigEazi <adesholatajudeen1@gmail.com>
2023-05-28 14:11:35 +0100

    refresh token, accessToken, cookies

---
+++ tut-11/controllers/employeesController.js
@@ -0,0 +1,95 @@
+const data = {
+  employees: require("../models/employees.json"),
+  setEmployees: function (data) {
+    this.employees = data;
+  },
+};
+
+const getAllEmployees = (req, res) => {
+  res.json(data.employees);
+};
+
+const createNewEmployee = (req, res) => {
+  // creating the new employee parameter for the request body
+  const newEmployee = {
+    id: data.employees[data.employees.length - 1].id + 1 || 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  };
+
+  // set the required condition for the two parameters
+  if (!newEmployee.firstname || !newEmployee.lastname) {
+    return res
+      .status(400)
+      .json({ message: "First name and last name are required" });
+  }
+
+  // add the new employee to the employee list
+  data.setEmployees([...data.employees, newEmployee]);
+
+  //   update the data
+  res.status(201).json(data.employees);
+};
+
+const updateEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+
+  if (req.body.firstname) employee.firstname = req.body.firstname; // if we found the id, go and set the firstname parameter value
+  if (req.body.lastname) employee.lastname = req.body.lastname;
+
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+
+  const unsortedArray = [...filteredArray, employee];
+
+  data.setEmployees(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+
+  res.json(data.employees);
+};
+
+const deleteEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found` });
+  }
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
+};
+
+const getEmployee = (req, res) => {
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.params.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.params.id} not found` });
+  }
+  res.json(employee);
+};
+
+module.exports = {
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee,
+};
---
+++ tut-11/controllers/logoutController.js
@@ -0,0 +1,45 @@
+const usersDB = {
+  users: require("../models/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs/promises");
+const path = require("path");
+const paths = require("path");
+const { json } = require("stream/consumers");
+
+const handleLogut = async (req, res) => {
+  //Also delete the token on client side
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); // No content
+  const refreshToken = cookies.jwt;
+
+  // Is the fresh token available in db?
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // clear cookies
+    return res.sendStatus(204);
+  }
+
+  // delete the refresh token if found user
+  const otherUsers = usersDB.users.find(
+    (person) => person.refreshToken !== refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "models", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  // delete cookie
+  res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // on production, {secure: true} must be added...it means only serve on https
+};
+
+module.exports = { handleLogut };
